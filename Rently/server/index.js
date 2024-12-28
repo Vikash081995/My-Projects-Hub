9766f6f9a8ef7bc8df766b59a64e685f89adbc69
Vikash kumar <vikash08.kumar1995@gmail.com>
2024-12-29 00:49:08 +0530

    o-auth2 setup

--- Rently/server/index.js
+++ Rently/server/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();  
-
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
-});
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const PORT = process.env.PORT || 5000;
+
+passport.use(new GoogleStrategy());
 
 app.listen(PORT,()=>{
     console.log("Server is running on port 5000");
