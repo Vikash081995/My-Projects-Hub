74a07bd1db1b6e7b2c10ff1a7bc3e4634d986ac7
Vikash kumar <vikash08.kumar1995@gmail.com>
2024-12-29 02:40:50 +0530

    o-auh2-setup-done

--- Rently/server/index.js
+++ Rently/server/index.js
@@ -1,13 +1,34 @@
-const express = require('express');
-const app = express();  
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+const express = require("express");
+const app = express();
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
-app.listen(PORT,()=>{
-    console.log("Server is running on port 5000");
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+  res.redirect('/')
 })
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 8080");
+});
