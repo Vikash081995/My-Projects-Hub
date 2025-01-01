fc8182be39eb5e32237655a505d04f4210de3141
Vikash kumar <vikash08.kumar1995@gmail.com>
2025-01-01 21:44:51 +0530

    cookies session setup

--- Rently/server/index.js
+++ Rently/server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const authRoutes = require("./routes/authRoutes");
 const keys = require("./config/keys");
 require("./models/User");
@@ -9,6 +11,17 @@ require("./services/passport");
 const PORT = process.env.PORT || 8080;
 
 authRoutes(app);
+
+//middleware
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(keys.mongoURI, {
   useNewUrlParser: true,
